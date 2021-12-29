813c5671580da68e386efe86a90cbebca87f89f1
Patrick Gallagher <paddy.gallagher@gmail.com>
2021-12-29 20:12:09 +0000

    Adding navigation links and routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/styles';
 
 import theme from './ui/Theme';
@@ -6,8 +7,20 @@ import Header from './ui/Header';
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      Hello!
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/services" element={<div>Services</div>} />
+          <Route path="/customsoftware" element={<div>Custom Software</div>} />
+          <Route path="/mobileapps" element={<div>Mobile Apps</div>} />
+          <Route path="/websites" element={<div>Websites</div>} />
+          <Route path="/revolution" element={<div>The Revolution</div>} />
+          <Route path="/about" element={<div>About Us</div>} />
+          <Route path="/contact" element={<div>Contact Us</div>} />
+          <Route path="/estimate" element={<div>Free Estimate</div>} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }

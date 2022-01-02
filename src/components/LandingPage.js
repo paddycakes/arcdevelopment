d3a1e004c0cdc4759451b79274934afa6c6b1d0a
Patrick Gallagher <paddy.gallagher@gmail.com>
2022-01-02 17:04:47 +0000

    Adding Custom Software Development section to Landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,9 +4,11 @@ import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import ButtonArrow from './ui/ButtonArrow';
 import animationData from '../animations/landinganimation/data';
+import customSoftwareIcon from '../assets/Custom Software Icon.svg';
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -33,16 +35,19 @@ const useStyles = makeStyles((theme) => ({
     marginTop: '1em',
   },
   learnButtonHero: {
-    borderColor: theme.palette.common.blue,
-    color: theme.palette.common.blue,
-    borderWidth: 2,
-    borderRadius: 50,
-    fontFamily: 'Roboto',
-    fontWeight: 'bold',
+    ...theme.typography.learnButton,
     fontSize: '0.9rem',
     height: 45,
     width: 145,
-    textTransform: 'none',
+  },
+  learnButton: {
+    ...theme.typography.learnButton,
+    fontSize: '0.7rem',
+    height: 35,
+    padding: 5,
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: '2em',
+    },
   },
   mainContainer: {
     marginTop: '5em',
@@ -60,11 +65,31 @@ const useStyles = makeStyles((theme) => ({
       marginLeft: 0,
     },
   },
+  specialText: {
+    fontFamily: 'Pacifico',
+    color: theme.palette.common.orange,
+  },
+  subtitle: {
+    marginBottom: '1em',
+  },
+  icon: {
+    marginLeft: '2em',
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0,
+    },
+  },
+  serviceContainer: {
+    marginTop: '12em',
+    [theme.breakpoints.down('sm')]: {
+      padding: 25,
+    },
+  },
 }));
 
 export default function LandingPage(props) {
   const classes = useStyles();
   const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
 
   const defaultOptions = {
     loop: true,
@@ -77,6 +102,7 @@ export default function LandingPage(props) {
 
   return (
     <Grid container direction="column" className={classes.mainContainer}>
+      {/* Hero block */}
       <Grid item>
         <Grid container direction="row" justify="flex-end" alignItems="center">
           <Grid sm item className={classes.heroTextContainer}>
@@ -109,6 +135,48 @@ export default function LandingPage(props) {
           </Grid>
           <Grid sm item className={classes.animation}>
             <Lottie options={defaultOptions} height={'100%'} width={'100%'} />
+          </Grid>
+        </Grid>
+      </Grid>
+
+      {/* Services block */}
+      <Grid item>
+        <Grid
+          container
+          direction="row"
+          justify={matchesSM && 'center'}
+          className={classes.serviceContainer}
+        >
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? 0 : '5em',
+              textAlign: matchesSM && 'center',
+            }}
+          >
+            <Typography variant="h4">Custom Software Development</Typography>
+            <Typography variant="subtitle1" className={classes.subtitle}>
+              Save Energy. Save Time. Save Money.
+            </Typography>
+            <Typography variant="subtitle1">
+              Complete digitial solutions, from investigation to{' '}
+              <span className={classes.specialText}>celebration.</span>
+            </Typography>
+            <Button variant="outlined" className={classes.learnButton}>
+              <span style={{ marginRight: 10 }}>Learn more</span>
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              />
+            </Button>
+          </Grid>
+          <Grid item>
+            <img
+              className={classes.icon}
+              alt="custom software icon"
+              src={customSoftwareIcon}
+            />
           </Grid>
         </Grid>
       </Grid>
